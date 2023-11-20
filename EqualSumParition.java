e3cf6eb24f509fdabb9d14a103e49d4b1ae26a1d
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2023-11-20 17:25:45 +0530

    Subset Problem Variayion

---
+++ EqualSumParition.java
@@ -0,0 +1,105 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class EqualSumParition {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    public static void main(String[] args) {
+        
+        FastReader input = new FastReader();
+        
+        int n = input.nextInt();
+
+        int[] a = new int[n];
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            a[i] = input.nextInt();
+            sum += a[i];
+        }
+
+        if(sum % 2 != 0){
+            System.out.println("False");
+        }else{
+            System.out.println(subsetSum(a, sum / 2));
+        }
+    }
+
+    static boolean subsetSum(int[] a, int x){
+
+        int n = a.length;
+
+        boolean[][] dp = new boolean[n + 1][x + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp.length; j++) {
+                if(i == 0){
+                    dp[i][j] = false;
+                }
+                if(j == 0){
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= x; j++) {
+                
+                int w = a[i - 1];
+
+                if(j >= w){
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - w];
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][x];
+    }
+}
