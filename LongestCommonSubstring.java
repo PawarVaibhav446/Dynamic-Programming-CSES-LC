08e1005b046eb407abb49536f7b05a83f316dc77
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2023-11-24 19:45:16 +0530

    LCS Variations

---
+++ LongestCommonSubstring.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class LongestCommonSubstring {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+ 
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    public static void main(String[] args) {
+        
+        FastReader input = new FastReader();
+        String text1 = input.next();
+        String text2 = input.next();
+
+        int n = text1.length();
+        int m = text2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for(int i = 0;i < n + 1;i++){
+            for(int j = 0;j < m + 1;j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        int maxLen = 0;
+
+        for(int i = 1;i < n + 1;i++){
+            for(int j = 1;j < m + 1;j++){
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    maxLen = Math.max(maxLen, dp[i][j]);              
+                }else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        System.out.println(maxLen);
+    }
+}
