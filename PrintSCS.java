af64bf8f62def8462fcf04060b1e12334bfea4a2
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2023-11-27 21:07:55 +0530

    SCS

---
+++ PrintSCS.java
@@ -0,0 +1,121 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class PrintSCS {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        FastReader input = new FastReader();
+        String s1 = input.next();
+        String s2 = input.next();
+
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        int i = m;
+        int j = n;
+
+        String result = "";
+
+        while(i > 0 && j > 0){
+            if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                result += s1.charAt(i - 1);
+                i--;
+                j--;
+            }else{
+                if(dp[i][j - 1] > dp[i - 1][j]){
+                    result += s2.charAt(j - 1);
+                    j--;
+                }else{
+                    result += s1.charAt(i - 1);
+                    i--;
+                }
+            }
+        }
+
+        while(i > 0){
+            result += s1.charAt(i - 1);
+            i--;
+        }
+
+        while(j > 0){
+            result += s2.charAt(j - 1);
+            j--;
+        }
+
+        StringBuilder sb = new StringBuilder(result);
+        sb.reverse();
+
+        String ans = sb.toString();
+
+        System.out.println(ans);
+    }
+}
